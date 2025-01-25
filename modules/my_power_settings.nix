92439507db9d6ff16ae3c4f50363f96a8250b4fa
htn332805 <96609281+htn332805@users.noreply.github.com>
2025-01-25 02:02:45 -0800

    Create my_power_settings.nix

---
+++ modules/my_power_settings.nix
@@ -0,0 +1,39 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Disable screen sleep
+  services.xserver.serverFlagsSection = ''
+    Option "BlankTime" "0"
+    Option "StandbyTime" "0"
+    Option "SuspendTime" "0"
+    Option "OffTime" "0"
+  '';
+
+  # Prevent hard drives from sleeping
+  powerManagement.powerUpCommands = ''
+    ${pkgs.hdparm}/sbin/hdparm -B 255 /dev/sda
+  '';
+
+  # Disable hibernation
+  boot.kernelParams = [ "nohibernate" ];
+
+  # Configure power button to shut down the system
+  services.logind.extraConfig = ''
+    HandlePowerKey=poweroff
+  '';
+
+  # Disable automatic suspend and hibernation
+  services.logind.lidSwitch = "ignore";
+  services.logind.lidSwitchExternalPower = "ignore";
+  powerManagement.enable = false;
+
+  # Disable GNOME power management
+  services.xserver.desktopManager.gnome = {
+    extraGSettingsOverrides = ''
+      [org.gnome.settings-daemon.plugins.power]
+      sleep-inactive-ac-type='nothing'
+      sleep-inactive-battery-type='nothing'
+      power-button-action='interactive'
+    '';
+  };
+}
