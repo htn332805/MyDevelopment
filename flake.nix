ecca664b9a8b2a2bdf9cf96191b92f8809d68e91
htn332805 <96609281+htn332805@users.noreply.github.com>
2025-01-24 17:16:23 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -55,6 +55,12 @@
           {
             myDisko.diskDevice = diskDevice;
           }
+          ({ modulesPath, ... }: {
+            imports = [
+              (modulesPath + "/installer/scan/not-detected.nix")
+              (modulesPath + "/profiles/qemu-guest.nix")
+            ];
+          })
         ];
       };
 
