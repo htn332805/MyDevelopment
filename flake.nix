cdcb5b3bd20b1ae29c3cf3b16d6726d955f4a488
htn332805 <96609281+htn332805@users.noreply.github.com>
2025-01-24 16:16:17 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Multi-platform NixOS configuration for Hai's setup";
+  description = "Multi-platform NixOS configuration with common development tools and Python packages";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -18,61 +18,92 @@
   let
     supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    mkSystem = import ./lib/mkSystem.nix;
   in
   {
     nixosConfigurations = {
-      # NixOS configurations
-      NixServ = nixpkgs.lib.nixosSystem {
+      X86Serv = mkSystem {
+        inherit nixpkgs home-manager;
         system = "x86_64-linux";
-        modules = [
-          ./hosts/x86_64-linux/configuration.nix
-          home-manager.nixosModules.home-manager
-        ]; #end of modules
-    }; #end of NixServe COnfig
+        extraModules = [ ./hosts/x86_64-linux/configuration.nix ];
+      };
 
-    Aarch64Serv = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        ./hosts/aarch64-linux/configuration.nix
-        home-manager.nixosModules.home-manager
-      ]; #end of modules
-    }; #end of Aarch64Serv
+      Aarch64Serv = mkSystem {
+        inherit nixpkgs home-manager;
+        system = "aarch64-linux";
+        extraModules = [ ./hosts/aarch64-linux/configuration.nix ];
+      };
 
-    Raspi4 = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        ./hosts/raspberry-pi/configuration.nix
-        home-manager.nixosModules.home-manager
-      ]; #end of Raspi4 modules
-    }; #end of raspi4 config
+      Raspi4B = mkSystem {
+        inherit nixpkgs home-manager;
+        system = "aarch64-linux";
+        extraModules = [ ./hosts/raspberry-pi/configuration.nix ];
+      };
 
-    wsl = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/wsl/configuration.nix
-        nixos-wsl.nixosModules.wsl
-        home-manager.nixosModules.home-manager
-      ]; #end of wsl modules
-    }; #end of wsl config
+      wsl = mkSystem {
+        inherit nixpkgs home-manager;
+        system = "x86_64-linux";
+        extraModules = [
+          ./hosts/wsl/configuration.nix
+          nixos-wsl.nixosModules.wsl
+        ];
+      };
+
+      RemInstall = mkSystem {
+        inherit nixpkgs home-manager;
+        system = "x86_64-linux";
+        extraModules = [
+          ({ modulesPath, ... }: {
+            imports = [
+              (modulesPath + "/installer/scan/not-detected.nix")
+              (modulesPath + "/profiles/qemu-guest.nix")
+            ];
+          })
+          ./hosts/remote/configuration.nix
+        ];
+      };
+    };
 
     darwinConfigurations = {
-      IntelMac= nixpkgs-darwin.lib.darwinSystem {
+      IntelMac = nixpkgs-darwin.lib.darwinSystem {
         system = "x86_64-darwin";
         modules = [
           ./hosts/darwin/configuration.nix
           home-manager.darwinModules.home-manager
-        ]; #end of intel mac modules
-      }; #end of intelmac config
+          ./modules/common.nix
+        ];
+      };
+
       MxMac = nixpkgs-darwin.lib.darwinSystem {
         system = "aarch64-darwin";
-          modules = [
-            ./hosts/darwin/configuration.nix
-            home-manager.darwinModules.home-manager
-          ]; #end of MxMac modules
-      }; #end of MxMac config
-    }; #end of darwin configs
-    # Other outputs
-    # Shared packages and modules
+        modules = [
+          ./hosts/darwin/configuration.nix
+          home-manager.darwinModules.home-manager
+          ./modules/common.nix
+        ];
+      };
+    };
+
+    devShell = forAllSystems (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        commonModule = import ./modules/common.nix;
+        pythonEnv = (pkgs.python3.withPackages (ps: with ps; [
+          jupyterlab
+          ipython
+          dash
+          matplotlib
+        ]));
+      in
+      pkgs.mkShell {
+        buildInputs = commonModule.environment.systemPackages ++ [ pythonEnv ];
+
+        shellHook = ''
+          echo "Development environment loaded with requested tools and Python packages."
+        '';
+      }
+    );
+
     packages = forAllSystems (system: {
       # Define your custom packages here
     });
@@ -84,5 +115,5 @@
     modules = {
       # Define your custom modules here
     };
-  }; #end of top nixosConfigurations
+  };
 }
