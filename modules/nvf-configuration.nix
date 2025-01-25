d31fe2a889644b0b3109988654392a362c99f449
htn332805 <96609281+htn332805@users.noreply.github.com>
2025-01-25 12:29:50 -0800

    Update nvf-configuration.nix

--- modules/nvf-configuration.nix
+++ modules/nvf-configuration.nix
@@ -5,14 +5,28 @@
       enable = true;
       name = "catppuccin";
       style = "dark";
-    }; end of theme
+    }; #end of theme
     statusline.lualine.enable = true;
     telescope.enable = true;
     autocomplete.nvim-cmp.enable = true;
     visuals.ident-blankline.enable = true;
+    languages = {
+      enableLSP = true;
+      enableTreesitter = true;
+      python = {
+        enable = true;
+        dap.enable = true;
+        format.neable = true;
+        lsp.enable = true;
+        lsp.server = "pyright";
+      }; #end of python
+      nix.enable = true;
+      ts.enable  = true
+      python.
+    }; #end of languages
   }; #end of vim
 
 
 
 
-}# top most brace
+} # top most brace
